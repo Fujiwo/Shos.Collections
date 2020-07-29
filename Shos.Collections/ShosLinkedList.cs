1b3307cbefb1e9944e13e0e8675cfae786ee6945
Fujio Kojima <fujiwo@shos.info>
2020-07-29 17:01:54 +0900

    First, Last, Add, AddLast, Find, AddAfter, AddBefore 追加

--- Shos.Collections/ShosLinkedList.cs
+++ Shos.Collections/ShosLinkedList.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 
@@ -6,7 +7,7 @@ namespace Shos.Collections
 {
     public class ShosLinkedList<TElement> : IEnumerable<TElement>
     {
-        class Node
+        public class Node
         {
             public TElement Value { get; set; }
             public Node? Next { get; set; } = null;
@@ -17,11 +18,11 @@ namespace Shos.Collections
             public Node(TElement value) => Value = value;
         }
 
-        Node? first = null;
+        public Node? First { get; private set; }  = null;
 
-        Node? Last {
+        public Node? Last {
             get {
-                for (var node = first; node != null; node = node.Next) {
+                for (var node = First; node != null; node = node.Next) {
                     if (node.Next == null)
                         return node;
                 }
@@ -31,11 +32,26 @@ namespace Shos.Collections
 
         public int Count { get; private set; } = 0;
 
+        public void Add(TElement element) => AddLast(element);
+
+        public void AddFirst(TElement element)
+        {
+            var newNode = new Node(element);
+            if (First == null) {
+                First = newNode;
+            } else {
+                Debug.Assert(Last != null);
+                newNode.Next = First;
+                First = newNode;
+            }
+            Count++;
+        }
+
         public void AddLast(TElement element)
         {
             var newNode = new Node(element);
-            if (first == null) {
-                first = newNode;
+            if (First == null) {
+                First = newNode;
             } else {
                 Debug.Assert(Last != null);
                 Last.Next = newNode;
@@ -43,12 +59,63 @@ namespace Shos.Collections
             Count++;
         }
 
+        public void AddAfter(Node node, TElement element)
+        {
+            if (node == null)
+                throw new ArgumentNullException();
+
+
+            var newNode  = new Node(element);
+            newNode.Next = node.Next;
+            node.Next    = newNode;
+            Count++;
+        }
+
+        public void AddBefore(Node node, TElement element)
+        {
+            if (node == null)
+                throw new ArgumentNullException();
+
+            var newNode = new Node(element);
+
+            var previousNode = PreviousNode(node);
+            if (previousNode == null) {
+                AddFirst(element);
+            } else {
+                Debug.Assert(object.ReferenceEquals(previousNode.Next, node));
+                newNode.Next = node;
+                previousNode.Next = newNode;
+            }
+            Count++;
+        }
+
+        public Node? Find(TElement element)
+        {
+            for (var node = First; node != null; node = node.Next) {
+                if (node.Value.Equals(element))
+                    return node;
+            }
+            return null;
+        }
+
         public IEnumerator<TElement> GetEnumerator()
         {
-            for (var node = first; node != null; node = node.Next)
+            for (var node = First; node != null; node = node.Next)
                 yield return node.Value;
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        Node? PreviousNode(Node node)
+        {
+            if (node == null)
+                throw new ArgumentNullException();
+
+            for (var previousNode = First; previousNode != null; previousNode = previousNode.Next) {
+                if (object.ReferenceEquals(previousNode.Next, node))
+                    return previousNode;
+            }
+            return null;
+        }
     }
 }
--- Shos.CollectionsTests/ShosLinkedListTests.cs
+++ Shos.CollectionsTests/ShosLinkedListTests.cs
@@ -1,22 +1,39 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Shos.Collections.Tests
 {
     using Shos.Collections;
 
+    static class AssertExtensions
+    {
+        public static void AreEqual<TElement>(IEnumerable<TElement> expects, IEnumerable<TElement> actuals)
+        {
+            var expectsList = expects.ToList();
+
+            var index = 0;
+            foreach (var element in actuals)
+                Assert.AreEqual(expectsList[index++], element);
+            Assert.AreEqual(expectsList.Count, index);
+        }
+    }
+
     [TestClass()]
     public class ShosLinkedListTests
     {
-        ShosLinkedList<int> linkedList = null;
+        ShosLinkedList<int>? linkedList = null;
 
         [TestInitialize]
         public void Setup() => linkedList = new ShosLinkedList<int>();
-
 
         [TestMethod()]
         public void Createできる()
         {
             Assert.AreEqual(0, linkedList.Count);
+            Assert.IsNull(linkedList.First);
+            Assert.IsNull(linkedList.Last);
         }
 
         [TestMethod()]
@@ -24,8 +41,17 @@ namespace Shos.Collections.Tests
         {
             linkedList.AddLast(100);
             Assert.AreEqual(1, linkedList.Count);
+            Assert.IsNotNull(linkedList.First);
+            Assert.AreEqual(100, linkedList.First.Value);
+            Assert.IsNotNull(linkedList.Last);
+            Assert.AreEqual(100, linkedList.Last.Value);
+            Assert.AreSame(linkedList.First, linkedList.Last);
+
             linkedList.AddLast(500);
             Assert.AreEqual(2, linkedList.Count);
+            Assert.AreEqual(100, linkedList.First.Value);
+            Assert.AreEqual(500, linkedList.Last.Value);
+            Assert.AreNotSame(linkedList.First, linkedList.Last);
         }
 
         [TestMethod()]
@@ -37,16 +63,93 @@ namespace Shos.Collections.Tests
             linkedList.AddLast(300);
             linkedList.AddLast(200);
             linkedList.AddLast(800);
+            AssertExtensions.AreEqual(new [] { 300, 200, 800 }, linkedList);
+        }
 
-            var index = 0;
-            foreach (var element in linkedList) {
-                switch (index++) {
-                    case 0: Assert.AreEqual(300, element); break;
-                    case 1: Assert.AreEqual(200, element); break;
-                    case 2: Assert.AreEqual(800, element); break;
-                    default: Assert.Fail(); break;
-                }
-            }
+        [TestMethod()]
+        public void AddFirstできる()
+        {
+            linkedList.AddFirst(300);
+            Assert.AreEqual(1, linkedList.Count);
+            Assert.AreEqual(300, linkedList.First.Value);
+            Assert.AreEqual(300, linkedList.Last.Value);
+
+            linkedList.AddLast(600);
+            Assert.AreEqual(2, linkedList.Count);
+            linkedList.AddFirst(100);
+            Assert.AreEqual(100, linkedList.First.Value);
+            Assert.AreEqual(600, linkedList.Last.Value);
+            AssertExtensions.AreEqual(new[] { 100, 300, 600 }, linkedList);
+        }
+
+        [TestMethod()]
+        public void Findできる()
+        {
+            Assert.IsNull(linkedList.Find(100));
+            linkedList.AddLast(200);
+            Assert.IsNull(linkedList.Find(100));
+            var node = linkedList.Find(200);
+            Assert.IsNotNull(node);
+            Assert.AreEqual(200, node.Value);
+            Assert.AreSame(node, linkedList.First);
+
+            linkedList.AddLast(800);
+            Assert.AreEqual(200, linkedList.Find(200).Value);
+            Assert.AreSame(linkedList.Find(200), linkedList.First);
+            Assert.AreEqual(800, linkedList.Find(800).Value);
+            Assert.AreSame(linkedList.Find(800), linkedList.Last);
+        }
+
+        [TestMethod()]
+        public void 初期化リストが使える()
+        {
+            linkedList = new ShosLinkedList<int> { 10, 60, 30 };
+            AssertExtensions.AreEqual(new[] { 10, 60, 30 }, linkedList);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddAfterでnullを渡すと例外が飛ぶ()
+        {
+            linkedList.AddAfter(null, 60);
+        }
+
+        [TestMethod()]
+        public void AddAfterできる()
+        {
+            linkedList = new ShosLinkedList<int> { 10 };
+            linkedList.AddAfter(linkedList.First, 70);
+            AssertExtensions.AreEqual(new[] { 10, 70 }, linkedList);
+
+            linkedList.AddAfter(linkedList.Last, 40);
+            AssertExtensions.AreEqual(new[] { 10, 70, 40 }, linkedList);
+
+            var node = linkedList.Find(70);
+            linkedList.AddAfter(node, 100);
+            AssertExtensions.AreEqual(new[] { 10, 70, 100, 40 }, linkedList);
+        }
+
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddBeforeでnullを渡すと例外が飛ぶ()
+        {
+            linkedList.AddBefore(null, 10);
+        }
+
+        [TestMethod()]
+        public void AddBeforeできる()
+        {
+            linkedList = new ShosLinkedList<int> { 10 };
+            linkedList.AddBefore(linkedList.First, 30);
+            AssertExtensions.AreEqual(new[] { 30, 10 }, linkedList);
+
+            linkedList.AddBefore(linkedList.Last, 90);
+            AssertExtensions.AreEqual(new[] { 30, 90, 10 }, linkedList);
+
+            var node = linkedList.Find(90);
+            linkedList.AddBefore(node, 110);
+            AssertExtensions.AreEqual(new[] { 30, 110, 90, 10 }, linkedList);
         }
     }
 }
