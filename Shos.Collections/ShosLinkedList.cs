111c44b3c604c59afc5383fa3e140f26910e351d
Fujio Kojima <fujiwo@shos.info>
2020-07-29 15:24:19 +0900

    Create と AddLast のテストを追加し、Count プロパティを追加

--- Shos.Collections/ShosLinkedList.cs
+++ Shos.Collections/ShosLinkedList.cs
@@ -5,7 +5,11 @@ namespace Shos.Collections
 {
     public class ShosLinkedList<TElement>
     {
+        public int Count { get; private set; } = 0;
+
         public void AddLast(TElement element)
-        { }
+        {
+            Count++;
+        }
     }
 }
--- Shos.CollectionsTests/ShosLinkedListTests.cs
+++ Shos.CollectionsTests/ShosLinkedListTests.cs
@@ -2,13 +2,30 @@
 
 namespace Shos.Collections.Tests
 {
+    using Shos.Collections;
+
     [TestClass()]
     public class ShosLinkedListTests
     {
+        ShosLinkedList<int> linkedList = null;
+
+        [TestInitialize]
+        public void Setup() => linkedList = new ShosLinkedList<int>();
+
+
         [TestMethod()]
-        public void AddLastTest()
+        public void Createできる()
         {
-            Assert.Fail();
+            Assert.AreEqual(0, linkedList.Count);
+        }
+
+        [TestMethod()]
+        public void AddLastできる()
+        {
+            linkedList.AddLast(100);
+            Assert.AreEqual(1, linkedList.Count);
+            linkedList.AddLast(500);
+            Assert.AreEqual(2, linkedList.Count);
         }
     }
 }
