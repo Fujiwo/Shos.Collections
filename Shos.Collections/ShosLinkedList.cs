7afd49b4046befb7692d8a5b9ed1fa2c8584bb44
Fujio Kojima <fujiwo@shos.info>
2020-07-29 15:07:33 +0900

    プロジェクト ファイルを追加します。

---
+++ Shos.Collections/ShosLinkedList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shos.Collections
+{
+    public class ShosLinkedList<TElement>
+    {
+        public void AddLast(TElement element)
+        { }
+    }
+}
---
+++ Shos.CollectionsTests/ShosLinkedListTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shos.Collections.Tests
+{
+    [TestClass()]
+    public class ShosLinkedListTests
+    {
+        [TestMethod()]
+        public void AddLastTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
